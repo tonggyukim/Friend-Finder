fe1681e1f837de209e50e14ce7e43c471a811438
Tonggyu Kim <x-zezima@hotmail.com>
2019-10-23 21:03:18 -0700

    finished app

---
+++ app/data/friends.js
@@ -0,0 +1,228 @@
+var friends = [
+{
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "jack",
+    "photo": "https://images.app.goo.gl/EugKY3NHcF2C7wgN6",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Afomia",
+    "photo": "https://github.com/settings/profile",
+    "scores": [
+    "3",
+    "3",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "Jane Doe",
+    "photo": "kdjfksdjfk",
+    "scores": [
+    "3",
+    "4",
+    "3",
+    "5",
+    "4",
+    "2",
+    "3",
+    "5",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Stephen",
+    "photo": "https://www.petmd.com/sites/default/files/Pit-Bull-Dog-Facts-1168732600_0.jpg",
+    "scores": [
+    "3",
+    "5",
+    "3",
+    "2",
+    "2",
+    "4",
+    "5",
+    "2",
+    "1",
+    "3"
+    ]
+    },
+    {
+    "name": "Ashlynn",
+    "photo": "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg",
+    "scores": [
+    "4",
+    "1",
+    "2",
+    "5",
+    "5",
+    "5",
+    "4",
+    "1",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Nnamdi",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwinuM7YnbPlAhWELn0KHRIXAHoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.gettyimages.com%2Fphotos%2Felon-musk&psig=AOvVaw0HpPvd243Nd1CpDKjEtUpk&ust=1571949320273404",
+    "scores": [
+    "5",
+    "2",
+    "5",
+    "4",
+    "3",
+    "2",
+    "4",
+    "2",
+    "3",
+    "4"
+    ]
+    },
+    {
+    "name": "James dimm",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjGyPXmsa7lAhUOd98KHQBBD7gQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.shutterstock.com%2Fcategory%2Fnature&psig=AOvVaw1Fb3OAD5wfcclfrdO1TRUw&ust=1571782920677109",
+    "scores": [
+    "3",
+    "4",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "4"
+    ]
+    },
+    {
+    "name": "Davontae Glenn",
+    "photo": "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/68585650_10219882980170672_563356532205420544_n.jpg?_nc_cat=104&_nc_oc=AQl-YJHdV71jdhWUuanHB19uLlSZouiQfKdrshsUhvJ4aRdLEGxo86HwJ_KxRs7K3I0&_nc_ht=scontent-sjc3-1.xx&oh=8e29b3cd38781e8bc333168ee1fd2161&oe=5E2A05B6",
+    "scores": [
+    "2",
+    "3",
+    "2",
+    "2",
+    "4",
+    "3",
+    "3",
+    "2",
+    "2",
+    "2"
+    ]
+    }
+    ];
+
+module.exports = friends; 
